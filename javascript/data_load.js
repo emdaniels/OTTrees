88dd90d8eaeb468bfa1fc2255f9fd24f6863b4d4
Emily Daniels <emily@emilydaniels.com>
2013-04-13 20:34:28 -0400

    uploading list of species again

--- javascript/data_load.js
+++ javascript/data_load.js
@@ -157,9 +157,9 @@ function load() {
 	$("#accordion > li > div").click(function(){
 		 
 	    if(false == $(this).next().is(':visible')) {
-	        $('#accordion ul').slideUp(300);
+	        $('#accordion ul').slideUp(600);
 	    }
-	    $(this).next().slideToggle(300);
+	    $(this).next().slideToggle(600);
 	});
 	 
 	$('#accordion ul:eq(0)').show();
