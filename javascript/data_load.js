cb0f1e9072623e6806fe8e92b9acaccc68de2085
Emily Daniels <emily@emilydaniels.com>
2013-04-26 15:34:49 -0400

    added transparent overlay on map to bring out tree color, adjusted tree size displayed to zoom level, adjusted tree colors

--- javascript/data_load.js
+++ javascript/data_load.js
@@ -1,15 +1,15 @@
-var initialLocation;
+//var initialLocation;
 var ottawa = new google.maps.LatLng(45.420353,-75.695982);
 var browserSupportFlag =  new Boolean();
-var useGeolocation = true;
+//var useGeolocation = true;
 var map = null;
-var zoomLevel = 15;
-var geocoder;
+var zoomLevel = 13;
+//var geocoder;
+bounds = new google.maps.LatLngBounds(
+	    new google.maps.LatLng(0,180), new google.maps.LatLng(180,0));
 var infowindow = new google.maps.InfoWindow({
 	size: new google.maps.Size(50,50)
 });
-var season = null;
-var dummyValue = new Date().getTime();
 var nameArray = new Array('Amur_Corktree', 'Apple', 'Ash', 'Ash_Black', 'Ash_European', 'Ash_Green', 'Ash_White', 'Aspen_Columnar', 'Aspen_Largetooth', 'Aspen_Trembling', 'Basswood', 'Beech', 'Beech_American', 'Beech_Blue', 'Beech_European', 'Birch', 'Birch_Gray', 'Birch_Weeping', 'Birch_White', 'Birch_Yellow', 'Buckeye_Ohio', 'Butternut', 'Catalpa_northern', 'Cedar_Eastern_White', 'Cherry', 'Cherry_Black', 'Cherry_Choke', 'Cherry_Choke_Schubert', 'Cherry_Purple_Leaf', 'Coffeetree_Kentucky', 'Crabapple', 'Elm', 'Elm_American', 'Elm_Prospector', 'Elm_Rock', 'Elm_Siberian', 'Fir', 'Fir_Balsam', 'Fir_Colorado', 'Fir_Douglas', 'Ginkgo', 'Hackberry', 'Hawthorn', 'Hazel_Turkish', 'Hemlock', 'Hickory_Bitternut', 'Hickory_Shagbark', 'Horsechestnut_Common', 'Ironwood', 'Juniper', 'Katsura_Tree', 'Larch_Eastern', 'Larch_European', 'Lilac_Japanese', 'Linden_Littleleaf', 'Locust_Black', 'Locust_Honey', 'Magnolia', 'Maple', 'Maple_Amur', 'Maple_Black', 'Maple_Freeman', 'Maple_Manitoba', 'Maple_Norway', 'Maple_Red', 'Maple_Silver', 'Maple_Sugar', 'Mountain_Ash_Oakleaf', 'Mountain_Ash_Showy', 'Mulberry', 'Oak', 'Oak_Bur', 'Oak_English', 'Oak_Pin', 'Oak_Red', 'Oak_White', 'Olive_Russian', 'Pear', 'Pine', 'Pine_Austrian', 'Pine_Jack', 'Pine_Red', 'Pine_Scotch', 'Pine_White', 'Poplar', 'Poplar_Balsam', 'Poplar_Lombardy', 'Redbud_Eastern', 'Serviceberry', 'Spruce', 'Spruce_Black', 'Spruce_Colorado', 'Spruce_Norway', 'Spruce_Red', 'Spruce_White', 'Sycamore_American', 'Unknown', 'Walnut_Black', 'Willow', 'Willow_Black', 'Willow_Weeping', 'Yew_Canada');
 var treeLayerArray = new Array();
 
@@ -31,11 +31,9 @@ var severeAllergenArray = new Array('Ash', 'Ash_Black', 'Ash_European', 'Ash_Gre
 var foodBearingArray = new Array('Apple', 'Butternut', 'Cherry', 'Cherry_Black', 'Cherry_Choke', 'Cherry_Choke_Schubert', 'Cherry_Purple_Leaf', 'Crabapple', 'Ginkgo', 'Hackberry', 'Hawthorn', 'Hazel_Turkish', 'Hickory_Shagbark', 'Mulberry', 'Olive_Russian', 'Pear', 'Serviceberry', 'Walnut_Black');
 var nonFoodBearingArray = new Array('Amur_Corktree', 'Ash', 'Ash_Black', 'Ash_European', 'Ash_Green', 'Ash_White', 'Aspen_Columnar', 'Aspen_Largetooth', 'Aspen_Trembling', 'Basswood', 'Beech', 'Beech_American', 'Beech_Blue', 'Beech_European', 'Birch', 'Birch_Gray', 'Birch_Weeping', 'Birch_White', 'Birch_Yellow', 'Buckeye_Ohio', 'Catalpa_northern', 'Cedar_Eastern_White', 'Coffeetree_Kentucky', 'Elm', 'Elm_American', 'Elm_Prospector', 'Elm_Rock', 'Elm_Siberian', 'Fir', 'Fir_Balsam', 'Fir_Colorado', 'Fir_Douglas', 'Hemlock', 'Hickory_Bitternut', 'Horsechestnut_Common', 'Ironwood', 'Juniper', 'Katsura_Tree', 'Larch_Eastern', 'Larch_European', 'Lilac_Japanese', 'Linden_Littleleaf', 'Locust_Black', 'Locust_Honey', 'Magnolia', 'Maple', 'Maple_Amur', 'Maple_Black', 'Maple_Freeman', 'Maple_Manitoba', 'Maple_Norway', 'Maple_Red', 'Maple_Silver', 'Maple_Sugar', 'Mountain_Ash_Oakleaf', 'Mountain_Ash_Showy', 'Oak', 'Oak_Bur', 'Oak_English', 'Oak_Pin', 'Oak_Red', 'Oak_White', 'Pine', 'Pine_Austrian', 'Pine_Jack', 'Pine_Red', 'Pine_Scotch', 'Pine_White', 'Poplar', 'Poplar_Balsam', 'Poplar_Lombardy', 'Redbud_Eastern', 'Spruce', 'Spruce_Black', 'Spruce_Colorado', 'Spruce_Norway', 'Spruce_Red', 'Spruce_White', 'Sycamore_American', 'Unknown', 'Willow', 'Willow_Black', 'Willow_Weeping', 'Yew_Canada');
 
-
-
 function initialize() { 
 	var myOptions = {
-			zoom: 12,
+			zoom: 13,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			mapTypeControl: true,
 			mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
@@ -45,6 +43,14 @@ function initialize() {
 	};
 
 	map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+
+	rect = new google.maps.Rectangle({
+	    bounds: bounds,
+		fillColor: "#000000",
+	    fillOpacity: 0.7,
+	    strokeWeight: 0,
+	    map: map
+	});
 }
 
 function toggleDataOn(treeName) {
@@ -83,88 +89,59 @@ function toggleArray(treeArray, toggleState) {
 	}
 }
 
-function calibrateSeason() {
-	var now = new Date();
-	var month = now.getMonth() + 1;
-
-	//early spring: April
-	if (month == 4) {
-		season = 1;
-	//late spring: May
-	} else if (month == 5) {
-		season = 2;
-	//summer: June-August
-	} else if ((month == 6) || (month == 7) || (month == 8)){
-		season = 3;
-	//early fall: September
-	} else if (month == 9) {
-		season = 4;
-	}
-	//late fall: October
-	else if (month == 10) {
-		season = 5;
-	}
-	//winter: November-March
-	else {
-		season = 6;  
-	}
-	return season;
-}
-
-//Loop through the results array and place a marker for each set of coordinates.
+//Loops through results array and places a marker for each set of coordinates
 window.tree_data = function(results) {
 	var markersArray = new Array();
+	var now = new Date();
+	var month = now.getMonth() + 1;
 
 	for (var i = 0; i < results.Placemark.length; i++) {
 		var coords = results.Placemark[i].Point.coordinates.split(',');
 		var latLng = new google.maps.LatLng(coords[1],coords[0]);
-		if (season == 1){
+		if (month == 4){
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
-				icon: getCircleEarlySpring(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH), results.earlySpringColor) //estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				//estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				icon: getCircleEarlySpring(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel, results.earlySpringColor)
 			});
-			markersArray.push(marker);
 		}
-		else if (season == 2){
+		else if (month == 5){
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
-				icon: getCircleLateSpring(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH), results.lateSpringColor) //estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				icon: getCircleLateSpring(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel, results.lateSpringColor)
 			});
-			markersArray.push(marker);
 		}
-		else if (season == 3){
+		else if ((month == 6) || (month == 7) || (month == 8)){
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
-				icon: getCircleSummer(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH), results.summerColor) //estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				icon: getCircleSummer(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel, results.summerColor)
 			});
-			markersArray.push(marker);
 		}
-		else if (season == 4){
+		else if (month == 9){
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
-				icon: getCircleEarlyFall(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH), results.earlyFallColor) //estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				icon: getCircleEarlyFall(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel, results.earlyFallColor)
 			});
-			markersArray.push(marker);
 		}
-		else if (season == 5){
+		else if (month == 10){
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
-				icon: getCircleLateFall(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH), results.lateFallColor) //estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				icon: getCircleLateFall(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel, results.lateFallColor)
 			});
-			markersArray.push(marker);
 		}
-		else {
+		else if ((month == 11) || (month == 12) || (month == 1)){
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
-				icon: getCircleWinter(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH), results.winterColor) //estimatedSpan = maxSpan * (estimatedAge) / matDBH
+				icon: getCircleWinter(parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel, results.winterColor)
 			});
 			markersArray.push(marker);
+			
 		}
 	}
 
@@ -175,10 +152,10 @@ function getCircleEarlySpring(estimatedSpan, earlySpringColor) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
 		fillColor: earlySpringColor,
-		fillOpacity: .2,
+		fillOpacity: .4,
 		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
+		strokeColor: earlySpringColor,
+		strokeWeight: .2
 	};
 }
 
@@ -186,10 +163,10 @@ function getCircleLateSpring(estimatedSpan, lateSpringColor) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
 		fillColor: lateSpringColor,
-		fillOpacity: .2,
+		fillOpacity: .4,
 		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
+		strokeColor: lateSpringColor,
+		strokeWeight: .2
 	};
 }
 
@@ -197,10 +174,10 @@ function getCircleSummer(estimatedSpan, summerColor) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
 		fillColor: summerColor,
-		fillOpacity: .2,
+		fillOpacity: .4,
 		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
+		strokeColor: summerColor,
+		strokeWeight: .2
 	};
 }
 
@@ -208,10 +185,10 @@ function getCircleEarlyFall(estimatedSpan, earlyFallColor) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
 		fillColor: earlyFallColor,
-		fillOpacity: .2,
+		fillOpacity: .4,
 		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
+		strokeColor: earlyFallColor,
+		strokeWeight: .2
 	};
 }
 
@@ -219,10 +196,10 @@ function getCircleLateFall(estimatedSpan, lateFallColor) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
 		fillColor: lateFallColor,
-		fillOpacity: .2,
+		fillOpacity: .4,
 		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
+		strokeColor: lateFallColor,
+		strokeWeight: .2
 	};
 }
 
@@ -230,10 +207,10 @@ function getCircleWinter(estimatedSpan, winterColor) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
 		fillColor: winterColor,
-		fillOpacity: .2,
+		fillOpacity: .4,
 		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
+		strokeColor: winterColor,
+		strokeWeight: .2
 	};
 }
 
@@ -253,7 +230,9 @@ function toTitleCase(str) {
 	return str.replace(/\w\S*/g, function(txt){
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 	});
+	
 }
+
 
 /* map loading function */
 /*
