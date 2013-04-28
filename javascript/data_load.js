fd9dcb09a1121df884360cfc4f005da7c3451e2b
Emily Daniels <emily@emilydaniels.com>
2013-04-28 15:13:13 -0400

    adding tree descriptions to info windows

--- javascript/data_load.js
+++ javascript/data_load.js
@@ -8,20 +8,6 @@ var season;
 bounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(0,180), new google.maps.LatLng(180,0));
 
-/*var infowindow = new google.maps.InfoWindow({
-	content: contentString,
-	size: new google.maps.Size(50,50)
-	});
-
-var contentString = '<div id="content">'+
-'<div id="siteNotice">'+
-'</div>'+
-'<h1 id="firstHeading" class="firstHeading">YES</h1>'+
-'<div id="bodyContent">'+
-'<p>Hello people of earth!</p>'+
-'</div>'+
-'</div>';*/
-
 //Tree Name Arrays
 var nameArray = new Array('Amur_Corktree', 'Apple', 'Ash', 'Ash_Black', 'Ash_European', 'Ash_Green', 'Ash_White', 'Aspen_Columnar', 'Aspen_Largetooth', 'Aspen_Trembling', 'Basswood', 'Beech', 'Beech_American', 'Beech_Blue', 'Beech_European', 'Birch', 'Birch_Gray', 'Birch_Weeping', 'Birch_White', 'Birch_Yellow', 'Buckeye_Ohio', 'Butternut', 'Catalpa_northern', 'Cedar_Eastern_White', 'Cherry', 'Cherry_Black', 'Cherry_Choke', 'Cherry_Choke_Schubert', 'Cherry_Purple_Leaf', 'Coffeetree_Kentucky', 'Crabapple', 'Elm', 'Elm_American', 'Elm_Prospector', 'Elm_Rock', 'Elm_Siberian', 'Fir', 'Fir_Balsam', 'Fir_Colorado', 'Fir_Douglas', 'Ginkgo', 'Hackberry', 'Hawthorn', 'Hazel_Turkish', 'Hemlock', 'Hickory_Bitternut', 'Hickory_Shagbark', 'Horsechestnut_Common', 'Ironwood', 'Juniper', 'Katsura_Tree', 'Larch_Eastern', 'Larch_European', 'Lilac_Japanese', 'Linden_Littleleaf', 'Locust_Black', 'Locust_Honey', 'Magnolia', 'Maple', 'Maple_Amur', 'Maple_Black', 'Maple_Freeman', 'Maple_Manitoba', 'Maple_Norway', 'Maple_Red', 'Maple_Silver', 'Maple_Sugar', 'Mountain_Ash_Oakleaf', 'Mountain_Ash_Showy', 'Mulberry', 'Oak', 'Oak_Bur', 'Oak_English', 'Oak_Pin', 'Oak_Red', 'Oak_White', 'Olive_Russian', 'Pear', 'Pine', 'Pine_Austrian', 'Pine_Jack', 'Pine_Red', 'Pine_Scotch', 'Pine_White', 'Poplar', 'Poplar_Balsam', 'Poplar_Lombardy', 'Redbud_Eastern', 'Serviceberry', 'Spruce', 'Spruce_Black', 'Spruce_Colorado', 'Spruce_Norway', 'Spruce_Red', 'Spruce_White', 'Sycamore_American', 'Unknown', 'Walnut_Black', 'Willow', 'Willow_Black', 'Willow_Weeping', 'Yew_Canada');
 var treeLayerArray = new Array();
@@ -65,10 +51,6 @@ function initialize() {
 		strokeWeight: 0,
 		map: map
 	});
-
-	/*google.maps.event.addListener(marker, 'click', function() {
-		  infowindow.open(map,marker);
-		});*/
 }
 
 function toggleDataOn(treeName) {
@@ -95,13 +77,6 @@ function toggleDataOff(treeName) {
 
 		treeLayerArray[treeName] = null;
 	}
-	
-	/*var i = overlays.length;
-    while (i--) {
-      var overlay = overlays[i];
-      if (overlay) overlay.setMap(null);
-      delete overlays[i];
-    }*/
 }
 
 function toggleArray(treeArray, toggleState) {
@@ -148,12 +123,10 @@ function changeSeason(seasonNumber) {
 	} else {
 		season = seasonNumber;
 	}
-
 	for (treeLayer in treeLayerArray) {
 		toggleDataOff(treeLayer);
 		toggleDataOn(treeLayer);
 	}
-	
 }
 
 //Array to loop through called tree species and display the correct season and location
@@ -166,140 +139,78 @@ window.tree_data = function(results) {
 			var latLng = new google.maps.LatLng(coords[1],coords[0]);
 			var zoomLevel = map.getZoom();
 			//estimatedSpan = maxSpan * (estimatedAge) / matDBH / zoomLevel
-			var estimatedSpan = parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) / parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel;
-			if (estimatedSpan > 80)
-				estimatedSpan = 80;
+			var estimatedSpan = parseInt(results.maxSpan) * (parseInt(results.Placemark[i].dbh) * parseInt(results.growthFactor)) / parseInt(results.matDBH) / zoomLevel / 10;
+			
+			if (estimatedSpan > 50)
+				estimatedSpan = 50;
 			else if (estimatedSpan < 1)
 				estimatedSpan = 1;
-
-			if (season == 1){
-				var marker = new google.maps.Marker({
-					position: latLng,
-					title: results.commonName,
-					map: map,
-					icon: getCircleEarlySpring(estimatedSpan, results.earlySpringColor)
-				});
-				markersArray.push(marker);
+			
+			var color = results.winterColor;
+			
+			switch(season)
+			{
+			case 1:
+				color = results.earlySpringColor;
+				break;
+			case 2:
+				color = results.lateSpringColor;
+				break;
+			case 3:
+				color = results.summerColor;
+				break;
+			case 4:
+				color = results.earlyFallColor;
+				break;
+			case 5:
+				color = results.lateFallColor;
+				break;
 			}
-			else if (season == 2){
-				var marker = new google.maps.Marker({
-					position: latLng,
-					title: results.commonName,
-					map: map,
-					icon: getCircleLateSpring(estimatedSpan, results.lateSpringColor)
-				});
-				markersArray.push(marker);
-			}
-			else if (season == 3){
-				var marker = new google.maps.Marker({
-					position: latLng,
-					title: results.commonName,
-					map: map,
-					icon: getCircleSummer(estimatedSpan, results.summerColor)
-				});
-				markersArray.push(marker);
-			}
-			else if (season == 4){
-				var marker = new google.maps.Marker({
-					position: latLng,
-					title: results.commonName,
-					map: map,
-					icon: getCircleEarlyFall(estimatedSpan, results.earlyFallColor)
-				});
-				markersArray.push(marker);
-			}
-			else if (season == 5){
-				var marker = new google.maps.Marker({
-					position: latLng,
-					title: results.commonName,
-					map: map,
-					icon: getCircleLateFall(estimatedSpan, results.lateFallColor)
-				});
-				markersArray.push(marker);
-			}
-			else if (season == 6){
-				var marker = new google.maps.Marker({
-					position: latLng,
-					title: results.commonName,
-					map: map,
-					icon: getCircleWinter(estimatedSpan, results.winterColor)
-				});
-				markersArray.push(marker);
-			}
+			
+			var contentString = '<div id="content">' +
+			'<h4>' + results.commonName + '</h4>' +
+			'Scientific Name: '+ results.scientificName + '<br/>' +
+			'<a href="' + results.enWiki + '"> En Wiki </a><br/> French Name: '+ results.frenchName +
+			'<a href="' + results.frWiki + '"> Fr Wiki </a><br/> Address: '+ results.Placemark.description +
+			'DBH: '+ results.Placemark.dbh +
+			'Estimated Age: '+ parseInt(results.Placemark.dbh * results.growthFactor) +
+			'Growth Rate: '+ results.growthRate +
+			'</div>';
+			
+			markersArray.push(createMarker(latLng, results.commonName, map, estimatedSpan, color, contentString));
 		}
 	}
-	
+
 	treeLayerArray[results.filename] = markersArray;
 }
 
+function createMarker(latLng, commonName, map, estimatedSpan, color, contentString) {
+	var marker = new google.maps.Marker({
+		position: latLng,
+		title: commonName,
+		map: map,
+		icon: getCircle(estimatedSpan, color),
+		clickable: true,
+		info: new google.maps.InfoWindow({
+			content: contentString
+		})
+	});
+	
+	google.maps.event.addListener(marker, 'click', function() {
+		marker.info.open(map, marker);
+	});
+	
+	return marker;
+}
+
 //Defines symbol for Early Spring
-function getCircleEarlySpring(estimatedSpan, earlySpringColor) {
+function getCircle(estimatedSpan, color) {
 	return {
 		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: earlySpringColor,
+		fillColor: color,
 		fillOpacity: .5,
 		scale: estimatedSpan,
-		strokeColor: earlySpringColor,
-		strokeWeight: .3
-	};
-}
-
-//Defines symbol for Late Spring
-function getCircleLateSpring(estimatedSpan, lateSpringColor) {
-	return {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: lateSpringColor,
-		fillOpacity: .5,
-		scale: estimatedSpan,
-		strokeColor: lateSpringColor,
-		strokeWeight: .3
-	};
-}
-
-//Defines symbol for Summer
-function getCircleSummer(estimatedSpan, summerColor) {
-	return {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: summerColor,
-		fillOpacity: .5,
-		scale: estimatedSpan,
-		strokeColor: summerColor,
-		strokeWeight: .3
-	};
-}
-
-//Defines symbol for Early Fall
-function getCircleEarlyFall(estimatedSpan, earlyFallColor) {
-	return {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: earlyFallColor,
-		fillOpacity: .5,
-		scale: estimatedSpan,
-		strokeColor: earlyFallColor,
-		strokeWeight: .3
-	};
-}
-
-//Defines symbol for Late Fall
-function getCircleLateFall(estimatedSpan, lateFallColor) {
-	return {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: lateFallColor,
-		fillOpacity: .5,
-		scale: estimatedSpan,
-		strokeColor: lateFallColor,
-		strokeWeight: .3
-	};
-}
-
-//Defines symbol for Winter
-function getCircleWinter(estimatedSpan, winterColor) {
-	return {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: winterColor,
-		fillOpacity: .5,
-		scale: estimatedSpan,
-		strokeColor: winterColor,
+		strokeColor: color,
 		strokeWeight: .3
 	};
 }
@@ -322,153 +233,3 @@ function toTitleCase(str) {
 	});
 
 }
-
-
-/* map loading function */
-/*
-var treeLayerArray = new Array();
-
-for (var i = 0; i < nameArray.length; i++) {
-	treeLayerArray[nameArray[i]] = new google.maps.KmlLayer('http://www.ottrees.com/data/trees/' + nameArray[i] + '.xml?dummy=' + dummyValue, {suppressInfoWindows: false, preserveViewport: true});
-}
-
- function load() {
-	var myOptions = {mapTypeId: google.maps.MapTypeId.ROADMAP, center: ottawa, disableDefaultUI: false};
-
-	 instantiate the map 
-	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-	geocoder = new google.maps.Geocoder();
-
-	 geolocation section 
-	if(navigator.geolocation && useGeolocation) { // Try W3C Geolocation (Preferred)
-		browserSupportFlag = true;
-		navigator.geolocation.getCurrentPosition(
-				function(position) {
-					initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-					map.setCenter(initialLocation);
-					var marker = new google.maps.Marker({map: map, position: initialLocation});
-				}, 
-				function() {
-					handleNoGeolocation(browserSupportFlag);
-				}
-		);
-	} else if (google.gears && useGeolocation) { // Try Google Gears Geolocation
-		browserSupportFlag = true;
-		var geo = google.gears.factory.create('beta.geolocation');
-		geo.getCurrentPosition(
-				function(position) {
-					initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
-					map.setCenter(initialLocation);
-					var marker = new google.maps.Marker({map: map, position: initialLocation});
-				}, 
-				function() {
-					handleNoGeoLocation(browserSupportFlag);
-				}
-		);
-	} else { // Browser doesn't support Geolocation
-		browserSupportFlag = false;
-		handleNoGeolocation(browserSupportFlag);
-	}
-
-	 default location assignment (if location not found) 
-	function handleNoGeolocation(errorFlag) {
-		if (errorFlag == true) {
-			initialLocation = ottawa;
-		} else {
-			initialLocation = ottawa;
-		}
-
-		map.setCenter(initialLocation);
-		var marker = new google.maps.Marker({map: map, position: initialLocation});
-	}
-
-	 default zoom level of map 
-	map.setZoom(zoomLevel);
-
-	//toggleKML(treeLayer, map);
-
-	//addKMLListener(treeLayer);
-
-	function addKMLListener(layer) {
-		google.maps.event.addListener(layer, 'click', function(kmlEvent) {
-			var name = kmlEvent.featureData.name;
-			var location = kmlEvent.latLng;
-			codeLatLng(name, location);
-		});
-	}
-
-	function codeLatLng(name, location) {
-		geocoder.geocode({'latLng': location}, function(results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				if (results[1]) {         
-					infowindow.setContent("<div id='content'>&nbsp;<br /><strong>" + toTitleCase(name) + "</strong><br />" + results[1].formatted_address + "<br /><a href='http://maps.google.com/maps?saddr=" + initialLocation + "&daddr=" + location.toString() + "&dirflg=w' target='_new'>Directions</a></div>" );
-					infowindow.setPosition(location);
-					infowindow.open(map);
-				}
-			} else {
-				alert("Geocoder failed due to: " + status);
-			}
-		});
-	}
-
-
-	// Process for determining icon size and age of tree based on DBH and species
-	//examines and extracts DBH of tree from KML file and extracts growthFactor, maxSpan and maxAge for that species from CSV file
-
-	//returns estimated span for individual tree
-	function estimateTreeSpan(){
-		var DBH = (KML.value);
-		var growthFactor = (CSV.value);
-		var maxSpan = (CSV.value);
-		var maxAge = (CSV value);
-		var estimatedAge= DBH * growthFactor;
-		var estimatedSpan = maxSpan * estimatedAge / maxAge;
-		return estimatedSpan;
-	}
-
-	//returns estimated age for individual tree
-	function estimateTreeAge(){
-		var DBH = (KML value);
-		var growthFactor = (CSV value);
-		var estimatedAge= DBH * growthFactor;
-		return estimatedAge;
-	}
-
-	//now have ability to add estimatedAge and estimatedSpan to tool-tip/info window for that tree
-	//alternately, could add an estimatedSpan column in CSV for each species with DBH 0-5, 6-10, 11-15, 16-20, 21-25, 26-30...etc. but highest DBH is around 130 for some trees
-
-	//adds estimatedSpan to symbol path for semi-accurate size on map
-	var earlySpringDTree(estimatedSpan) {
-		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: 'green',
-		fillOpacity: .2,
-		scale: estimatedSpan,
-		strokeColor: 'white',
-		strokeWeight: .5
-	};
-
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(45.420353,-75.695982),
-		icon: earlySpringDTree,
-		map: map
-	});
-
-	//for displaying icons for each tree in the KML file		
-	for(var i = 0; i < latitudes.length; i++) {
-		var latLng = new google.maps.LatLng(latitudes[i], longitudes[i])
-		var newCircle = new google.maps.Marker({
-			icon: earlySpringDTree,
-			position: latLng
-		});
-		newCircle.setMap(map);
-	}
-} 
-
-function toggleKML(kmlLayer, map) {
-	if (kmlLayer.getMap() == null && map != null) {
-		kmlLayer.setMap(map);
-	} else if (kmlLayer.getMap() != null && map == null) {
-		kmlLayer.setMap(null);
-	}
-}*/
